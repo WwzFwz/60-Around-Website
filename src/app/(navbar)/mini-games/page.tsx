9460588dab5f66b0c78233b93a90cc77f661365c
WwzFwz <dzakyaureliafawwaz@gmail.com>
2024-08-24 10:30:28 +0700

    chore : folder structuring

---
+++ src/app/(navbar)/mini-games/page.tsx
@@ -0,0 +1,26 @@
+
+import React from "react";
+
+
+// export default function Page() {
+
+//   return (
+//     <div>
+//       <div ><h1>jsjsj   </h1>
+
+
+//       </div>
+//     </div>
+//   );
+// }
+
+const MiniGamesPage = () => {
+    return (
+      <div>
+        <h1>Welcome to Mini Games</h1>
+      </div>
+    );
+  };
+  
+export default MiniGamesPage;
+  
