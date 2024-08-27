bf0603276fe91d7623a63f1925646dd6843611c7
WwzFwz <dzakyaureliafawwaz@gmail.com>
2024-08-27 17:04:58 +0700

    feat : added drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
-import { type Config } from "drizzle-kit";
+import { defineConfig } from 'drizzle-kit';
 
-import { env } from "~/env";
-
-export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "postgresql",
+export default defineConfig({
+  schema: './src/server/db/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql', // PostgreSQL
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: 'aws-0-ap-southeast-1.pooler.supabase.com', // Host dari DATABASE_URL
+    port: 6543, // Port dari DATABASE_URL
+    user: 'postgres.cbcbfvvlzsalhunyonzv', // User dari DATABASE_URL
+    password: '4378yrhfuinel;mklfn3', // Password dari DATABASE_URL
+    database: 'postgres', // Database dari DATABASE_URL
+    ssl: false, // Menggunakan SSL untuk koneksi
   },
-  tablesFilter: ["6O-Website_*"],
-} satisfies Config;
+});
+
