bc0aec0cbcca50831338d2edf90a93d4cee3f021
Ahmad Wafi Idzharulhaqq <167051609+Awfidz@users.noreply.github.com>
2024-08-28 23:49:22 +0700

    feat : home page code

--- src/app/(navbar)/home/page.tsx
+++ src/app/(navbar)/home/page.tsx
@@ -1,12 +1,45 @@
 import React from "react";
+import Image from "next/image";
+import bcImage from "../public/Home_Bc.png";
+import Link from "next/link"
 
-
-export default function Page() {
-
+export default function Home() {
   return (
-    //contoh//
-     <div className="bg-green-500 h-screen" >Page  
-    </div>
+    <div
+      style={{
+        backgroundImage: `url(${bcImage.src})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "100vw",
+        height: "100vh",
+        display: 'flex',
+        justifyContent: 'flex-start', 
+        position: 'relative' 
+      }}
+    >
+        <div className="z-20 flex flex-col mt-80 ml-40 ">
+          <h1 className="text-3xl md:text-5xl font-bold mb-6 mt-8">Embrace The Moment</h1>
+          <h1 className="text-3xl md:text-5xl font-bold mb-4">Around Campus!</h1>
+          <p className="text-lg md:text-xl mb-1">
+            GO AROUND Campus of Ganesha ITB and create extraordinary experiences with us.
+          </p>
+        </div>
+        <div
+          className="absolute bottom-0 w-screen opacity-60"
+        >
+          <Link href="/eksplore">
+            <p className="text-blue-900 font-bold py-4"
+                style={{
+                  background: 'linear-gradient(to top, #FFC475, #F9E998)',
+                  textAlign: 'center',
+                  padding: '10px',
+                }}
+                >
+              Ready to Explore?
+            </p>
+          </Link>
+        </div>
+      </div>
   );
 }
-
